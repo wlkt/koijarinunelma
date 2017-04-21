cbf213f151371b699454008a428052eed9863a66
Ville Lehti <ville.lehti1@edu.turkuamk.fi>
2017-04-21 23:56:52 +0300

    Added user image upload with Formidable

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 var express = require('express');
+var formidable = require('formidable');
+
+// Kirjautumisen varmistus
+var ensureAuthd = require('./ensureAuthd.js'); 
+
 var router = express.Router();
 
 // Hae kotisivu
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/', function(req, res){
 	
 	res.render('index');
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	else {
-		req.flash('error_msg', 'Et ole kirjautunut sisään.'); 
-		res.redirect('/users/login');
-	}
-}
+router.get('/edit', ensureAuthd.ensureAuthd, function(req, res){
+    var now = new Date();
+    res.render('edit', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+router.post('/edit', ensureAuthd.ensureAuthd, function(req, res){
+    var form = new formidable.IncomingForm();
+	form.uploadDir = './public/img/gallery';
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error'); // error puuttuu
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/edit');
+    });
+});
+
 
 module.exports = router;
