0bc2dcc9885b7c5394f83112af94230c029ea671
Attegates <laakso95@gmail.com>
2017-04-23 16:35:15 +0300

    Changed images to pop-up instead of opening to a new tab on click.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,19 @@
+
 // Kaikki img elementit joilla on class image
+// Kun kuvaa painetaan näyttää kuvan pop-uppina.
 $("img.image").click(function(eventInfo){
-    eventInfo.preventDefault();
-    // Haetaan kuvan polku
-    var imgPath = eventInfo.target.src;
-    // Avataan kuva uuteen välilehteen
-    window.open(imgPath);
+    var modal = $("#modalImgDiv")[0];
+    var img = eventInfo.target;
+    var modalImg = $("#modalImg")[0];
+
+    // Asetetaan kuvan lähde ja näytetään se.
+    modal.style.display = "block";
+    modalImg.src = img.src;
+});
+
+// Span elementti, jolla modal image suljetaan
+$("span.close").click(function(eventInfo) {
+    var modal = $("#modalImgDiv")[0];
+    // Piilotetaan modal image
+    modal.style.display = "none";
 });
